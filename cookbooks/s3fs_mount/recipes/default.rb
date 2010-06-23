d23137c59f8150819c7e3f6b5c5bfa795db13a15
aakash <aakashd@gmail.com>
2010-06-23 09:49:20 +0530

    adding log and config s3 mounts

--- cookbooks/s3fs_mount/recipes/default.rb
+++ cookbooks/s3fs_mount/recipes/default.rb
@@ -25,15 +25,8 @@ directory "/mnt/s3cache" do
   mode 0775
 end
 
-directory "/shared" do
-  owner node[:owner_name]
-  group node[:owner_name]
-  mode 0775
-  not_if {File.directory?("/shared")}
-end
-
-bucket = "#{node[:environment][:name]}-s3fs-#{Digest::SHA1.hexdigest(node[:aws_secret_id])[0..6]}"
-
+log_bucket = "#{node[:environment][:name]}-shared-logs-#{Digest::SHA1.hexdigest(node[:aws_secret_id])[0..6]}"
+config_bucket = "#{node[:environment][:name]}-shared-config-#{Digest::SHA1.hexdigest(node[:aws_secret_id])[0..6]}"
 
 ruby_block "make-s3-bucket" do
   block do
@@ -43,20 +36,29 @@ ruby_block "make-s3-bucket" do
                                         :access_key_id     => node[:aws_secret_id],
                                         :secret_access_key => node[:aws_secret_key]
                                         )
-    
-    begin
-      AWS::S3::Bucket.create bucket
-    rescue AWS::S3::ResponseError
+    [log_bucket, config_bucket].each do |bucket|
+      begin
+        AWS::S3::Bucket.create bucket
+      rescue AWS::S3::ResponseError
+      end
     end
   end
 end
 
-bash "add-s3fs-to-fstab" do
-  code "echo 's3fs##{bucket} /shared fuse allow_other,accessKeyId=#{node[:aws_secret_id]},secretAccessKey=#{node[:aws_secret_key]},use_cache=/mnt/s3cache 0 0' >> /etc/fstab"
-  not_if "grep 's3fs##{bucket}' /etc/fstab"
+bash "add-logs-to-fstab" do
+  code "rmdir /data/choruscard/shared/log"
+  code "echo 's3fs##{log_bucket} /data/choruscard/shared/log fuse allow_other,accessKeyId=#{node[:aws_secret_id]},secretAccessKey=#{node[:aws_secret_key]},use_cache=/mnt/s3cache 0 0' >> /etc/fstab"
+  not_if "grep 's3fs##{log_bucket}' /etc/fstab"
+end
+
+bash "add-logs-to-fstab" do
+  code "rmdir /data/choruscard/shared/config"
+  code "echo 's3fs##{config_bucket} /data/choruscard/shared/config fuse allow_other,accessKeyId=#{node[:aws_secret_id]},secretAccessKey=#{node[:aws_secret_key]},use_cache=/mnt/s3cache 0 0' >> /etc/fstab"
+  not_if "grep 's3fs##{config_bucket}' /etc/fstab"
 end
 
 bash "maybe-start-s3fs" do
-  code "/usr/bin/s3fs #{bucket} /shared -ouse_cache=/mnt/s3cache -oallow_other"
+  code "/usr/bin/s3fs #{log_bucket} /data/choruscard/shared/log -ouse_cache=/mnt/s3cache -oallow_other"
+  code "/usr/bin/s3fs #{config_bucket} /data/choruscard/shared/config -ouse_cache=/mnt/s3cache -oallow_other"
   not_if "ps -A | grep s3fs"
 end
